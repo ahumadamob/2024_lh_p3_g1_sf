c6b102dee4c9e3964ba51c88ea4e566e27bd7b57
MartinEmmSuarez <tyncho.wary@gmail.com>
2024-05-28 20:20:34 -0300

    ya se puede persistir entidad Categoria

---
+++ src/main/java/Grupo1/G1P3LH/entity/Categoria.java
@@ -0,0 +1,32 @@
+package Grupo1.G1P3LH.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
